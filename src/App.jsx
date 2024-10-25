8aab08530aae7584710b8b0eb8c7636763c3694b
Talha <talhaiftikhar12549@gmail.com>
2024-10-26 03:21:29 +0500

    Rotation

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ import MainBoard from "./Components/MainBoard"
 function App() {
 
     return (
-        <>
-            <Header/>
-            <MainBoard/>
-        </>
+        // <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{
+                transform: 'rotate(0deg)',
+                // // transformOrigin: 'top left',
+                //  height: '100%',
+                // width: '100%'
+            }}>
+                <Header />
+                <MainBoard />
+            </div>
+        // </div>
     )
 }
 
